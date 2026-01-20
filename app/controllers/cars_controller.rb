ef90441ea978a7fd7bb55a691a42afc2875b3b04
Roland Lopez <rolandlopez.developer@gmail.com>
2026-01-20 13:07:08 +0700

    Update search feature with typesense

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,9 +1,12 @@
 class CarsController < ApplicationController
   def index
-    @pagy, @cars = pagy(:offset, Car.all, limit: 12)
+    @pagy, @cars = Car.search(params[:q], "make,model,vehicle_style", {
+      per_page: 12,
+      page: params[:page] || 1
+    })
 
     if turbo_frame_request?
-      render partial: "page", locals: { pagy: @pagy, cars: @cars }
+      render partial: "page", locals: { pagy: @pagy, cars: @cars, q: params[:q] }
     end
   end
 end
